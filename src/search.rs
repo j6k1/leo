a62e82be7669b10af4650ccf1c5287df64fbd863
j6k1 <jinpu@s254.xrea.com>
2022-10-16 10:28:29 +0900

    startup_strategyメソッドの定義を追加

--- src/search.rs
+++ src/search.rs
@@ -2,15 +2,16 @@ use std::marker::PhantomData;
 use std::ops::{Add, Neg, Sub};
 use std::sync::{Arc, Mutex};
 use std::sync::atomic::{AtomicBool, AtomicU64};
+use std::sync::mpsc::Sender;
 use std::time::{Duration, Instant};
 use usiagent::command::UsiInfoSubCommand;
 use usiagent::event::{UserEventDispatcher, UserEventQueue};
-use usiagent::hash::KyokumenMap;
+use usiagent::hash::{KyokumenHash, KyokumenMap};
 use usiagent::logger::Logger;
 use usiagent::OnErrorHandler;
 use usiagent::player::InfoSender;
-use usiagent::rule::{AppliedMove, State};
-use usiagent::shogi::{MochigomaCollections, ObtainKind, Teban};
+use usiagent::rule::{AppliedMove, LegalMove, State};
+use usiagent::shogi::{MochigomaCollections, MochigomaKind, ObtainKind, Teban};
 use crate::error::{ApplicationError, SendSelDepthError};
 use crate::nn::Evalutor;
 use crate::solver::Solver;
@@ -118,6 +119,61 @@ pub trait Search<L,S> where L: Logger + Send + 'static, S: InfoSender {
             Ok(())
         }
     }
+
+    fn startup_strategy<'a>(&self,env:&'a mut Environment<L,S>, gs:&'a mut GameState<'a,L,S>, m:LegalMove, priority:u32)
+                            -> Option<(u32,Option<ObtainKind>,u64,u64,KyokumenMap<u64,()>,KyokumenMap<u64,u32>,bool)> {
+
+        let obtained = match m {
+            LegalMove::To(ref m) => m.obtained(),
+            _ => None,
+        };
+
+        let mut oute_kyokumen_map = gs.oute_kyokumen_map.clone();
+        let mut current_kyokumen_map = gs.current_kyokumen_map.clone();
+
+        let (mhash,shash) = {
+            let o = gs.obtained.and_then(|o| MochigomaKind::try_from(o).ok());
+
+            let mhash = env.hasher.calc_main_hash(gs.mhash,gs.teban,gs.state.get_banmen(),gs.mc,m.to_applied_move(),&o);
+            let shash = env.hasher.calc_sub_hash(gs.shash,gs.teban,gs.state.get_banmen(),gs.mc,m.to_applied_move(),&o);
+
+            if priority == 10 {
+                match oute_kyokumen_map.get(gs.teban,&mhash,&shash) {
+                    Some(_) => {
+                        return None;
+                    },
+                    None => {
+                        oute_kyokumen_map.insert(gs.teban,mhash,shash,());
+                    },
+                }
+            }
+
+            (mhash,shash)
+        };
+
+        if priority < 10 {
+            oute_kyokumen_map.clear(gs.teban);
+        }
+
+        let depth = match priority {
+            5 | 10 => gs.depth + 1,
+            _ => gs.depth,
+        };
+
+        let is_sennichite = match current_kyokumen_map.get(gs.teban,&mhash,&shash).unwrap_or(&0) {
+            &c if c >= 3 => {
+                return None;
+            },
+            &c if c > 0 => {
+                current_kyokumen_map.insert(gs.teban,mhash,shash,c+1);
+
+                true
+            },
+            _ => false,
+        };
+
+        Some((depth,obtained,mhash,shash,oute_kyokumen_map,current_kyokumen_map,is_sennichite))
+    }
 }
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum EvaluationResult {
@@ -170,6 +226,7 @@ pub struct Environment<L,S> where L: Logger, S: InfoSender {
     event_queue:Arc<Mutex<UserEventQueue>>,
     info_sender:S,
     on_error_handler:Arc<Mutex<OnErrorHandler<L>>>,
+    hasher:Arc<KyokumenHash<u64>>,
     limit:Option<Instant>,
     current_limit:Option<Instant>,
     adjust_depth:bool,
@@ -186,18 +243,19 @@ impl<L,S> Clone for Environment<L,S>
           S: InfoSender {
     fn clone(&self) -> Self {
         Environment {
-            event_queue:self.event_queue.clone(),
+            event_queue:Arc::clone(&self.event_queue),
             info_sender:self.info_sender.clone(),
-            on_error_handler:self.on_error_handler.clone(),
+            on_error_handler:Arc::clone(&self.on_error_handler),
+            hasher:Arc::clone(&self.hasher),
             limit:self.limit.clone(),
             current_limit:self.current_limit.clone(),
             adjust_depth:self.adjust_depth,
             network_delay:self.network_delay,
             display_evalute_score:self.display_evalute_score,
-            stop:self.stop.clone(),
-            quited:self.quited.clone(),
+            stop:Arc::clone(&self.stop),
+            quited:Arc::clone(&self.quited),
             kyokumen_score_map:self.kyokumen_score_map.clone(),
-            nodes:self.nodes.clone(),
+            nodes:Arc::clone(&self.nodes),
             think_start_time:self.think_start_time.clone()
         }
     }
@@ -206,6 +264,7 @@ impl<L,S> Environment<L,S> where L: Logger, S: InfoSender {
     pub fn new(event_queue:Arc<Mutex<UserEventQueue>>,
                info_sender:S,
                on_error_handler:Arc<Mutex<OnErrorHandler<L>>>,
+               hasher:Arc<KyokumenHash<u64>>,
                think_start_time:Instant,
                limit:Option<Instant>,
                current_limit:Option<Instant>,
@@ -220,6 +279,7 @@ impl<L,S> Environment<L,S> where L: Logger, S: InfoSender {
             event_queue:event_queue,
             info_sender:info_sender,
             on_error_handler:on_error_handler,
+            hasher:hasher,
             think_start_time:think_start_time,
             limit:limit,
             current_limit:current_limit,
@@ -270,6 +330,25 @@ impl<L,S> Root<L,S> where L: Logger + Send + 'static, S: InfoSender {
 impl<L,S> Search<L,S> for Root<L,S> where L: Logger + Send + 'static, S: InfoSender {
     fn search<'a>(&self, env: &mut Environment<L, S>, gs: GameState<'a, L, S>, evalutor: Evalutor)
         -> Result<EvaluationResult, ApplicationError> {
+        todo!()
+    }
+}
+pub struct Recursive<L,S> where L: Logger + Send + 'static, S: InfoSender {
+    l:PhantomData<L>,
+    s:PhantomData<S>,
+    sender:Sender<(AppliedMove,i32)>
+}
+impl<L,S> Recursive<L,S> where L: Logger + Send + 'static, S: InfoSender {
+    pub fn new(sender:Sender<(AppliedMove,i32)>) -> Recursive<L,S> {
+        Recursive {
+            l:PhantomData::<L>,
+            s:PhantomData::<S>,
+            sender:sender
+        }
+    }
+}
+impl<L,S> Search<L,S> for Recursive<L,S> where L: Logger + Send + 'static, S: InfoSender {
+    fn search<'a>(&self, env: &mut Environment<L, S>, gs: GameState<'a, L, S>, evalutor: Evalutor) -> Result<EvaluationResult, ApplicationError> {
         todo!()
     }
 }
